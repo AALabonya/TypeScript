2bf4bf5ffdecb0136ff47a2185f94ddcd74e4284
Labonya <1aklimakter@gmail.com>
2024-01-06 10:14:03 +0600

    added scope

--- first-program/index.js
+++ first-program/index.js
@@ -3,3 +3,15 @@ userName = "labonya";
 var age = 25;
 console.log(age);
 console.log(userName);
+// Global scope
+let globalVar = 'I am global';
+
+function myFunction() {
+  // Local scope
+  let localVar = 'I am local';
+  console.log(globalVar); // Accessible
+  console.log(localVar); // Accessible
+}
+
+// console.log(globalVar); // Accessible
+// console.log(localVar); // Not accessible - ReferenceError: localVar is not defined
--- first-program/index.ts
+++ first-program/index.ts
@@ -1,8 +1,8 @@
 let userName: string 
 userName= "labonya";
 let age = 25;
-
-let country = "Bangladesh"
+let country: string
+ country = "Bangladesh"
 console.log(country);
 
 console.log(age)
